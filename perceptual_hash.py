a2e389a7d82ec41488d481b9d21c7ca9b0eb237e
CryptoCollectNetwork <shaneloyer2021@outlook.com>
2026-08-27 01:11:24 -0400

    updat

--- perceptual_hash.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import imagehash
-
-filename = r"hhOoYZtHBqQi3d-dmxcGooXKTbiT3HJ2-eNsE7HNtKg.mp4"
-
-image = Image.open(filename)
-
-print("dHash:", imagehash.dhash(image))
-print("aHash:", imagehash.average_hash(image))
